daf32642da935c6feb55835fe85331f551259a02
Briana Dodgen <brianadodgen@gmail.com>
2023-03-05 12:53:39 +0100

    created BaseTest

--- tests/unit/models/test_item.py
+++ tests/unit/models/test_item.py
@@ -3,10 +3,15 @@ from tests.unit.models.base_item_test import BaseItemTest
 
 class TestItem(BaseItemTest):
     def test_create_item(self):
-        self.assertEqual(self.dummy_item.name, 'Test Item')
-        self.assertEqual(self.dummy_item.price, 73.57)
+        def err_msg(property: str) -> str:
+            return f"The {property} of the item after creation does not equal the constructor argument."
 
-    def test_json(self):
+        self.assertEqual(self.dummy_item.name, 'Test Item', err_msg("name"))
+        self.assertEqual(self.dummy_item.price, 73.57, err_msg("price"))
+
+    def test_item_json(self):
         expected = {'name': 'Test Item', 'price': 73.57}
+        test_input = self.dummy_item.json()
+        err_msg = f"The JSON export of the item is incorrect. Received: {test_input}, Expected: {expected}"
 
-        self.assertDictEqual(expected, self.dummy_item.json())
+        self.assertDictEqual(expected, test_input, err_msg)
